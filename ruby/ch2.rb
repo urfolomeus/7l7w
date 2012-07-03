beed6c60f0ceeb92301bcc624a5669bab1b3f3dc
Alan Gardner <urfolomeus@gmail.com>
2012-07-03 22:46:17 +0100

    First hack at part 2 of chapter 2 of the ruby section

--- ruby/ch2.rb
+++ ruby/ch2.rb
@@ -32,4 +32,36 @@ SliceTest.new.with_slice.result
 
 # part 2
 
+class Tree
+  attr_accessor :children, :node_name, :tree
+
+  def self.from_hash(hash)
+    top_node = hash.first
+    name, children = top_node.first, top_node.last
+    new(name, children)
+  end
+
+  def initialize(name, children={})
+    @children = children.map {|p, c| Tree.new(p, c)}
+    @node_name = name
+  end
+
+  def visit_all(&block)
+    visit &block
+    children.each {|c| c.visit_all &block}
+  end
+
+  def visit(&block)
+    block.call self
+  end
+end
+
+fam_hash = {'grandpa' => { 'dad' => {'child 1' => {}, 'child 2' => {} }, 'uncle' => {'child 3' => {}, 'child 4' => {} } } }
+
+family_tree = Tree.from_hash(fam_hash)
+
+puts "*"*20
+family_tree.visit {|node| puts node.node_name}
+puts "*"*20
+family_tree.visit_all {|node| puts node.node_name}
 
